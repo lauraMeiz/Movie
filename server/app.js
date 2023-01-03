06478fb545330a36c8345e969387843ea4cdc244
Laura <97021990+lauraMeiz@users.noreply.github.com>
2023-01-03 14:37:06 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3005;
+const cors = require("cors");
+app.use(cors());
+
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "movie",
+});
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/movies-manager", (req, res) => {
+  // SELECT column1, column2, ...			<----------------------
+  // FROM table_name;				<----------------------
+  const sql = `					
+    SELECT						
+    *						
+    FROM movies					
+    `;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
